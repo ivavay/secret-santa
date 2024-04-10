cb90bf8a3d18670d9340c3d8085ac375f7758016
Ivy Chen <ichen@ucsc.edu>
2024-04-10 12:21:22 +0800

    first part of form validation done

--- client/src/components/MakeSession.tsx
+++ client/src/components/MakeSession.tsx
@@ -2,9 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'
 import { useState } from 'react';
+import { useForm } from "react-hook-form"
 
 function MakeSession(this: any) {
     const [startDate, setStartDate] = useState(new Date());
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm()
+    const onSubmit = (data) => console.log(data)
+
     return(
         <>
         <div className="background d-flex justify-content-center">
@@ -12,7 +20,7 @@ function MakeSession(this: any) {
                 <div className="card-body">
                 <h5 className="card-title">Organize a session</h5>
                 <p className="card-text">Let's get you set up for a gift giving session! Be sure to set the budget that each gift should be, the date, and invite the guests, and we'll take care of the rest.</p>
-                <form>
+            
                     <div className="mb-3">
                     <label className="form-label">Session Date</label>
                     <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} minDate={new Date()}/>
@@ -25,21 +33,26 @@ function MakeSession(this: any) {
                     <div className="form-card">
                     <h5 className="card-title">Invite guests</h5>
                         <div className="mb-3">
-                            <label className="form-label">First Name</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                            </div>
-                            <div className="mb-3">
-                            <label className="form-label">Last Name</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                            </div>
-                            <div className="mb-3">
-                            <label className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                            </div>
-                            <button type="submit" className="btn border-success space-btns">Add person</button>   
-                            <button type="submit" className="btn btn-success space-btns">Save</button>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                        <input placeholder="First Name"
+                                {...register("firstName", { pattern: /^[A-Za-z]+$/i})}
+                                aria-invalid={errors.firstName ? "true" : "false"}
+                            />
+                        {errors.firstName && <p role="alert">First name is required</p>}
+                        <input placeholder="Last Name"{...register("lastName", { pattern: /^[A-Za-z]+$/i})} 
+                            aria-invalid={errors.lastName ? "true" : "false"}/>
+                            {errors.lastName && <p role="alert">Last name is required</p>}
+                        <input placeholder="Email"
+                            {...register("email", { pattern: /^[^\s@]+@[^\s@]+\.(com|net|edu|gmail)$/ })}
+                            aria-invalid={errors.email ? "true" : "false"}
+                        />
+                        {errors.email && <p role="alert">Email is required</p>}
+                        <input type="submit" />
+                        </form>
+                        </div>
+                        <button type="submit" className="btn border-success space-btns">Add person</button>   
+                        <button type="submit" className="btn btn-success space-btns">Save</button>
                     </div>
-                    </form>
                 </div>
             </div>
         </div>
